afeb8c003ba9bd51ddc3b6e94577b6e3828df1c7
Alexandra Materan <alexandrafigueroa02@gmail.com>
2026-08-24 23:58:37 -0500

    Fix: agregar espera explícita en ReservePage para evitar fallos por timing en CI

--- pages/reserve_page.py
+++ pages/reserve_page.py
@@ -1,10 +1,14 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class ReservePage:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
         self.choose_flight_button = (By.CLASS_NAME, "btn-small")
 
     def click_choose_flight(self):
+        self.wait.until(EC.presence_of_element_located(self.choose_flight_button))
         self.driver.find_element(*self.choose_flight_button).click()
